c98a82fb5f5ebe80e773c15c1e3d0ecfe91654da
Paul Renwick <prenwick@gmail.com>
2019-04-26 13:10:36 +1200

    Populated database, added footer, other css

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,22 +1,26 @@
 import React from 'react'
-import TestQuestion from './TestQuestion'
 import Question from './Question'
 import Header from './Header'
 import Footer from './Footer'
 import StartQuiz from './StartQuiz'
 
 // Material UI imports
-import Typography from '@material-ui/core/Typography'
 import Grid from '@material-ui/core/Grid'
 
 const App = () => (
-  <Grid>
+
+  <Grid
+    container
+    direction="row"
+    justify="center"
+    alignitems="center"
+    className="display">
     <StartQuiz />
     <Header />
     <Question />
-    <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
-    <Footer />
+
   </Grid>
+
 )
 
 export default App
